955eb42ab5dcbe6fcd2c8c3b7e7bcf71463c688a
Ritu Singh <69470113+RituSingh2002@users.noreply.github.com>
2022-07-29 17:57:58 +0530

    smtp0

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ let btn=document.getElementById('button');
 btn.addEventListener('click',sendEmail)
 function sendEmail(){
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "ritusingh29g@gmail.com",
-        Password : "Ritu@2002",
-        To : 'ritusingh29g@gmail.com',
-        From :document.getElementById("email").value,
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert("mail sent successfully")
-    );
+        
+      SecureToken : '9b0fa1d9-e7e0-4aca-96ae-05c19a556ad6',
+      To : 'ritusingh29g@gmail.com',
+      From : "1900460100068ritusingh@gmail.com",
+      Subject : "This is the subject",
+      Body : "And this is the body"
+  }).then(
+    message => alert(message)
+  );
 }
  
